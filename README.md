7b72e4cb5f7c5ffe9aa3f39fd8242630f47fdf81
maar-ten <maar-ten@users.noreply.github.com>
2023-12-23 11:57:40 +0000

    implement spatial graph

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# self-driving-car
-Pet project about a car that autonomously navigates a virtual world
+# Self Driving Car
+Is a pet project about a car that autonomously navigates a virtual world.
+
+This project follows the video series of [Radu Mariescu-Istodor](https://www.youtube.com/@Radu)
+
+## Run the project
+If you have NodeJS installed simply run `npx http-server` inside the project dir.
---
+++ js/math/graph.js
@@ -0,0 +1,63 @@
+class Graph {
+    constructor(points = [], segments = []) {
+        this.points = points;
+        this.segments = segments;
+    }
+
+    addPoint(point) {
+        this.points.push(point);
+    }
+
+    containsPoint(point) {
+        return this.points.find(p => p.equals(point));
+    }
+
+    tryAddPoint(point) {
+        if (!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
+    removePoint(point) {
+        const segments = this.getSegmentsWithPoint(point);
+        segments.forEach(seg => this.removeSegment(seg));
+        this.points.splice(this.points.indexOf(point), 1);
+    }
+
+    addSegment(seg) {
+        this.segments.push(seg);
+    }
+
+    containsSegment(seg) {
+        return this.segments.find(s => s.equals(seg));
+    }
+
+    tryAddSegment(seg) {
+        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    }
+
+    removeSegment(seg) {
+        this.segments.splice(this.segments.indexOf(seg), 1);
+    }
+
+    getSegmentsWithPoint(point) {
+        return this.segments.filter(seg => seg.includes(point));
+    }
+
+    dispose() {
+        this.points.length = 0;
+        this.segments.length = 0;
+    }
+
+    draw(ctx) {
+        this.segments.forEach(seg => seg.draw(ctx));
+        
+        this.points.forEach(point => point.draw(ctx));
+    }
+}
