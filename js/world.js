b06485816f03dc45e43f44eb7b6ee7e901d27818
maar-ten <maar-ten@users.noreply.github.com>
2023-12-23 20:42:22 +0000

    implement building generation

--- js/world.js
+++ js/world.js
@@ -1,11 +1,22 @@
 class World {
-    constructor(graph, roadWidth = 100, roadRoundness = 10) {
+    constructor(
+        graph,
+        roadWidth = 100, 
+        roadRoundness = 10,
+        buildingWidth = 150,
+        buildingMinLength = 150,
+        spacing = 50
+    ) {
         this.graph = graph;
         this.roadWidth = roadWidth;
         this.roadRoundness = roadRoundness;
+        this.buildingWidth = buildingWidth;
+        this.buildingMinLength = buildingMinLength;
+        this.spacing = spacing;
 
         this.envelops = [];
         this.roadBorders = [];
+        this.buildings = [];
         
         this.generate();
     }
@@ -14,6 +25,11 @@ class World {
         this.envelops.length = 0;
         this.envelops = this.graph.segments.map(s => new Envelop(s, this.roadWidth, this.roadRoundness));
         this.roadBorders = Polygon.union(this.envelops.map(e => e.poly));
+        this.buildings = this.#generateBuildings(); 
+    }
+
+    #generateBuildings() {
+        // todo implement https://youtu.be/BBFNvmUKxJ0?si=6HmbQylILes9krlf&t=218
     }
 
     draw(ctx) {
