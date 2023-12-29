4f0c337d641833d3f8c41aee1c08176114669dc0
maar-ten <maar-ten@users.noreply.github.com>
2023-12-29 10:25:40 +0000

    add lanes to the road

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * .9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -11,6 +12,13 @@ function animate() {
     car.update();
 
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * .7);
+
+    road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
