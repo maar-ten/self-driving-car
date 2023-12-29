a0b2f7e3ad317b6f013eff12bf211c8944e243d9
maar-ten <maar-ten@users.noreply.github.com>
2023-12-29 09:15:04 +0000

    implement moving of car

--- car.js
+++ car.js
@@ -7,13 +7,18 @@ class Car {
 
         this.speed = 0;
         this.acceleration = .2;
-        this.maxSpeed = 3;
+        this.maxSpeed = 5;
         this.friction = .05;
+        this.angle = 0;
 
         this.controls = new Controls();
     }
 
     update() {
+        this.#move();
+    }
+
+    #move() {
         if (this.controls.forward) {
             this.speed += this.acceleration;
         }
@@ -26,23 +31,45 @@ class Car {
         if (this.speed < -this.maxSpeed / 2) {
             this.speed = -this.maxSpeed / 2;
         }
-        if (this.speed > 0 ) {
+        if (this.speed > 0) {
             this.speed -= this.friction;
         }
-        if (this.speed < 0 ) {
+        if (this.speed < 0) {
             this.speed += this.friction;
         }
-        this.y -= this.speed;
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+
+            if (this.controls.left) {
+                this.angle += .03 * flip;
+            }
+            if (this.controls.right) {
+                this.angle -= .03 * flip;
+            }
+        }
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            -this.width / 2,
+            -this.height / 2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
