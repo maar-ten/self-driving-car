282c97b161a245374ded2842db86f2451b536d97
maar-ten <maar-ten@users.noreply.github.com>
2024-01-01 17:35:52 +0000

    save and reload the brain

--- car.js
+++ car.js
@@ -106,7 +106,7 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx, color) {
+    draw(ctx, color, drawSensor = false) {
         if (this.damaged) {
             ctx.fillStyle = 'grey';
         } else {
@@ -119,7 +119,7 @@ class Car {
         }
         ctx.fill();
 
-        if (this.sensor) {
+        if (this.sensor && drawSensor) {
             this.sensor.draw(ctx);
         }
     }
--- main.js
+++ main.js
@@ -6,30 +6,55 @@ networkCanvas.width = 300;
 const carCtx = carCanvas.getContext('2d');
 const networkCtx = networkCanvas.getContext('2d');
 const road = new Road(carCanvas.width / 2, carCanvas.width * .9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'AI');
+const cars = generateCars(100);
+let bestCar = cars[0];
+if (localStorage.getItem('bestBrain')) {
+    bestCar.brain = JSON.parse(localStorage.getItem('bestBrain'));
+}
+
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)
 ];
 
 animate();
 
+function save() {
+    localStorage.setItem(
+        'bestBrain',
+        JSON.stringify(bestCar.brain)
+    );
+}
+
+function discard() {
+    localStorage.removeItem('bestBrain');
+}
+
+function generateCars(n) {
+    return Array(n).fill().map(_ => new Car(road.getLaneCenter(1), 100, 30, 50, 'AI'));
+}
+
 function animate(time) {
     traffic.forEach(car => car.update(road.borders, []));
-    car.update(road.borders, traffic);
+    cars.forEach(c => c.update(road.borders, traffic));
+
+    const bestCar = cars.find(c => c.y === Math.min(...cars.map(c => c.y)));
 
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
 
     carCtx.save();
-    carCtx.translate(0, -car.y + carCanvas.height * .7);
+    carCtx.translate(0, -bestCar.y + carCanvas.height * .7);
 
     road.draw(carCtx);
     traffic.forEach(car => car.draw(carCtx, 'red'));
-    car.draw(carCtx, 'blue');
+    carCtx.globalAlpha = .2;
+    cars.forEach(c => c.draw(carCtx, 'blue'));
+    carCtx.globalAlpha = 1;
+    bestCar.draw(carCtx, 'blue', true);
 
     carCtx.restore();
 
     networkCtx.lineDashOffset = -time / 100;
-    Visualizer.drawNetwork(networkCtx, car.brain);
+    Visualizer.drawNetwork(networkCtx, bestCar.brain);
     requestAnimationFrame(animate);
 }
